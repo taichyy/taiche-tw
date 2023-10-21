4deb1411ccb871a3a84c2ed6af975cda1347be8c
taichyy <chinzeilulu@gmail.com>
2023-10-22 02:11:18 +0800

    Fix wrong link

--- app/(roots)/portfolio/page.jsx
+++ app/(roots)/portfolio/page.jsx
@@ -10,7 +10,7 @@ const Portfolio = () => {
     <div className="w-screen h-screen flex items-center justify-center">
         <div className="text-center">
             本頁面正在重新建置中，請先至
-            <a href="https://taiche-tw-old.vercel.app/Portfolio" target="_new" className=" text-red-950 font-bold text-lg">舊站</a>
+            <a href="https://taiche-tw-old.vercel.app/portfolio" target="_new" className=" text-red-950 font-bold text-lg">舊站</a>
             <br/>
             或
             <Link href="/resume" className=" text-red-950 font-bold text-lg">個人簡歷</Link>、<Link href="/about" className=" text-red-950 font-bold text-lg">關於我</Link>
