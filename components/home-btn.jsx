54d05908cd9c89aa6505da42c950a8fb48b259bf
taichyy <chinzeilulu@gmail.com>
2023-10-23 19:14:42 +0800

    Fix nav covered issue

--- components/home-btn.jsx
+++ components/home-btn.jsx
@@ -7,7 +7,7 @@ const HomeBtn = ({type, className}) => {
         flex items-center justify-center 
         mt-8 ml-8 p-3 
         duration-500 font-semibold md:font-medium bg-[#EBFF00] text-slate-950 text-xl md:text-2xl
-        hover:text-white hover:bg-slate-950
+        hover:text-white hover:bg-slate-950 z-50
         ${type=="fixed" ? "fixed" : ""} ${className}`
     }>
         <h3><Link href="/">HOME</Link></h3>
