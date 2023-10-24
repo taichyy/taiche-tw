9b520329a3c60e13cefaa51c193e6f473e204455
taichyy <chinzeilulu@gmail.com>
2023-10-24 19:31:44 +0800

    Make portfolio much better, but still in the lack of data

--- app/(roots)/portfolio/page.jsx
+++ app/(roots)/portfolio/page.jsx
@@ -23,14 +23,16 @@ const Portfolio = () => {
             <HomeBtn type="fixed" className="hidden md:flex"/>
             <Hamburger />
         </nav>
-        <main className="px-5 mb-10 md:px-72 md:grid">
-            <h2 className="mt-28 md:mt-40 text-center text-3xl tracking-wider font-bold text-slate-600 border-b-2 w-fit pb-2 mx-auto border-slate-600 px-3">實務作品</h2>
-        </main>
-        <div className="
-            flex mx-6 flex-col md:mx-14 mb-10 gap-2
+        <div className="px-5 mb-10 md:px-72 md:grid">
+            <h2 className="mt-28 md:mt-40 text-center text-3xl tracking-wider font-bold text-slate-600 border-b-2 w-fit pb-2 mx-auto border-slate-600 px-3">
+                實務作品
+            </h2>
+        </div>
+        <main className="
+            flex mx-6 flex-col md:mx-14 mb-2 gap-2
             sm:grid sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 
         ">
-            {portfolio.map( (item,index) => (
+            {portfolio.real.map( (item,index) => (
                 <Card className="w-11/12 mx-auto mb-10" key={index}>
                     <CardHeader>
                         <CardTitle className="text-xl">
@@ -42,7 +44,7 @@ const Portfolio = () => {
                                     {line}
                                 </span>
                             ))}
-                            <span className="break-all">
+                            <span className="break-all text-sm">
                                 {item.language}
                             </span>
                         </CardDescription>
@@ -54,7 +56,7 @@ const Portfolio = () => {
                                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                                 alt={`${item.title} Image`}
                                 src={item.img}
-                                className=""
+                                className="hover:opacity-90 duration-200"
                             />
                         </div>
                     </CardContent>
@@ -63,7 +65,50 @@ const Portfolio = () => {
                     </CardFooter>
                 </Card>
             ))}
+        </main>
+        <div className="px-5 mb-10 md:px-72 md:grid">
+            <h2 className="mt-28 md:mt-16 text-center text-3xl tracking-wider font-bold text-slate-600 border-b-2 w-fit pb-2 mx-auto border-slate-600 px-3">
+                其他作品
+            </h2>
         </div>
+        <main className="
+            flex mx-6 flex-col md:mx-14 mb-10 gap-2
+            sm:grid sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 
+        ">
+            {portfolio.others.map( (item,index) => (
+                <Card className="w-11/12 mx-auto mb-10" key={index}>
+                    <CardHeader>
+                        <CardTitle className="text-xl">
+                            {item.title}
+                        </CardTitle>
+                        <CardDescription>
+                            {item.subtitle.split('^t').map( (line, index) => (
+                                <span key={line} className="block">
+                                    {line}
+                                </span>
+                            ))}
+                            <span className="break-all text-sm">
+                                {item.language}
+                            </span>
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <div className="relative w-full rounded-sm overflow-hidden aspect-video">
+                            <Image
+                                fill
+                                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                                alt={`${item.title} Image`}
+                                src={item.img}
+                                className="hover:opacity-90 duration-200"
+                            />
+                        </div>
+                    </CardContent>
+                    <CardFooter className="flex justify-between">
+                        <PortfolioMore data={item}/>
+                    </CardFooter>
+                </Card>
+            ))}
+        </main>
     </div>
     );
 }
--- components/portfolio-more.jsx
+++ components/portfolio-more.jsx
@@ -13,61 +13,68 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import { Separator } from "./ui/separator";
 
 const PortfolioMore = ({data}) => {
 
-    // http://findmatthew.com/
-    
     return ( 
-    <Dialog className="">
+    <Dialog>
         <DialogTrigger>
             <div className="cursor-pointer border-2 border-[#e31b6d] mt-10 py-1 px-5 hover:bg-[#e31b6d] duration-200 hover:text-white">
                 了解更多
             </div>
         </DialogTrigger>
-        <DialogContent className="w-11/12">
-            <DialogHeader>
-                <DialogTitle className="text-left text-xl">
-                    {data.title}
-                </DialogTitle>
-                <DialogDescription className="text-left">
-                    {data.subtitle.split('^t').map( (line, index) => (
-                        <div key={line}>
+        <DialogContent className="flex flex-row">
+            <div className="mt-5">
+                <DialogHeader>
+                    <DialogTitle className="text-left text-xl">
+                        {data.title}
+                    </DialogTitle>
+                    <DialogDescription className="text-left">
+                        {data.subtitle.split('^t').map( (line, index) => (
+                            <div key={line}>
+                                {line}
+                            </div>
+                        ))}
+                        <br/>
+                        開發語言：{data.language}
+                    </DialogDescription>
+                </DialogHeader>
+                <Separator className="my-2"/>
+                <div>
+                    {data.body.split('^t').map( (line, index) => (
+                        <div key={index}>
                             {line}
                         </div>
                     ))}
-                    <br/>
-                    開發語言：{data.language}
-                </DialogDescription>
-            </DialogHeader>
-            <Separator />
-            <div>
-                {data.body.split('^t').map( (line, index) => (
-                    <div key={index}>
-                        {line}
-                    </div>
-                ))}
-            </div>
-            <DialogFooter className="sm:justify-start">
-                <DialogClose asChild>
-                    {data.link != "" ? (
-                        <a href={data.link} target="_blank">
+                </div>
+                <DialogFooter className="sm:justify-start mt-3">
+                    <DialogClose asChild>
+                        {data.link != "" ? (
+                            <a href={data.link} target="_blank">
+                                <Button type="button" variant="secondary" className="flex text-white w-fit px-6 bg-[#e31b6d] rounded-none hover:bg-white hover:text-[#e31b6d] border-0">
+                                    <ArrowUpRightSquare width={18} className="mr-1 mt-1"/>
+                                    參觀作品
+                                </Button>
+                            </a>
+                        ) : (
                             <Button type="button" variant="secondary" className="flex text-white w-fit px-6 bg-[#e31b6d] rounded-none hover:bg-white hover:text-[#e31b6d] border-0">
-                                <ArrowUpRightSquare width={18} className="mr-1 mt-1"/>
-                                參觀作品
+                                <X width={18} className="mr-1"/>
+                                本項目暫無連結
                             </Button>
-                        </a>
-                    ) : (
-                        <Button type="button" variant="secondary" className="flex text-white w-fit px-6 bg-[#e31b6d] rounded-none hover:bg-white hover:text-[#e31b6d] border-0">
-                            <X width={18} className="mr-1"/>
-                            本項目暫無連結
-                        </Button>
-                    )}
-                </DialogClose>
-            </DialogFooter>
+                        )}
+                    </DialogClose>
+                </DialogFooter>
+            </div>
+            <div className="mt-5">
+                <Image
+                    src={data.img}
+                    alt={`${data.img.split('/')[data.img.split('/').length-1]} Image`}
+                    width={500}
+                    height={500}
+                    className="aspect-video hover:opacity-95 duration-200"
+                />
+            </div>
         </DialogContent>
     </Dialog>
     );
