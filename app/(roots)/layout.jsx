c52107f032aee873809a860ffb663db2920debdd
taichyy <chinzeilulu@gmail.com>
2024-07-21 14:12:27 +0800

    Update: Readme, and some data update.

---
+++ app/(roots)/layout.jsx
@@ -0,0 +1,10 @@
+const RoutesLayout = ({children}) => {
+
+    return (
+    <>
+        {children}
+    </>
+    );
+}
+ 
+export default RoutesLayout;
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,19 +1,18 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 
-// iconmoon
+// ico-moon
 import '../public/icomoon/style.css';
 
-
 export const metadata = {
-  title: "Tai's taiche.tw | HOME",
-  description: 'Tai-Cheng, Yen － 個人介紹、作品集、履歷網頁',
+    title: "Tai's taiche.tw | HOME",
+    description: 'Tai-Cheng, Yen － 個人介紹、作品集、履歷網頁',
 }
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className="font-sans min-h-screen bg-[#F3F3F3]">{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className="font-sans min-h-screen bg-[#F3F3F3]">{children}</body>
+        </html>
+    )
 }
