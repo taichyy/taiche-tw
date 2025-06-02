b0432d151ff921c2a6736be6cf36f2140c52d5ae
taichyy <chinzeilulu@gmail.com>
2025-06-02 15:43:35 +0800

    Update: Email.

--- app/(routes)/(others)/contact/page.jsx
+++ app/(routes)/(others)/contact/page.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Contact = () => {
     const data = {
-        contact_mail: "taichedev@gmail.com",
+        contact_mail: "chinzeilulu@gmail.com",
     }
     
     return (
