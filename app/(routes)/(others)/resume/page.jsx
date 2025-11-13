1be0070720a9fa63a6a3e7984c81a9c2c808b2e7
taichyy <chinzeilulu@gmail.com>
2025-11-13 03:19:08 +0000

    Update: Some text desc.

--- app/(routes)/(others)/resume/page.jsx
+++ app/(routes)/(others)/resume/page.jsx
@@ -7,29 +7,30 @@ import JobBlock from "@/components/job-block";
 import ResumePercent from "@/components/resume-percent/resume-percent";
 
 const Resume = () => {
-
     const data = {
-        resume_name : "嚴太成",
+        resume_name: "嚴太成",
         resume_edu: [
             {
                 edu_duration: ["2019/09", "2023/07"],
                 edu_school: "國立高雄科技大學（第一校區）",
                 edu_dept: "資訊管理系",
-            }
+            },
         ],
         resume_exp: [
             {
                 exp_duration: ["2024/04", ""],
                 exp_title: "高級前端網頁工程師",
                 exp_corporation: "誠諾工程技術股份有限公司",
-                exp_desc: "獨立開發公司 ERP 系統，負責 架構設計、前端開發、API 設計、資料庫前期規劃，技術範圍涵蓋 Next.js、TypeScript\n從 0 到 1 打造完整前端架構，開發超過 100+ 頁面、3 大模組（人資系統、倉儲管理、日常作業）\n超過萬行程式碼的系統設計與優化，確保系統穩定性與可維護性\n獨立負責 ERP 商業化專案，目前系統已正式運行並對外販售\n"
+                exp_desc:
+                    "獨立開發公司 ERP 系統，負責 架構設計、前端開發、API 設計、資料庫前期規劃，技術範圍涵蓋 Next.js、TypeScript\n從 0 到 1 打造完整前端架構，開發超過 100+ 頁面、3 大模組（人資系統、倉儲管理、日常作業）\n超過萬行程式碼的系統設計與優化，確保系統穩定性與可維護性\n獨立負責 ERP 商業化專案，目前系統已正式運行並對外販售\n",
             },
             {
                 exp_duration: ["2023/12", "2024/03"],
                 exp_title: "軟體工程師",
                 exp_corporation: "富有玩具有限公司（NX樂園）",
-                exp_desc: "開發海報自動生成系統，整合 Adobe AI 產生海報，降低 80% 製作時間，提升設計效率\n開發 Line LIFF 會員中心前端頁面，提升會員管理體驗\n"
-            }
+                exp_desc:
+                    "開發海報自動生成系統，整合 Adobe AI 產生海報，降低 80% 製作時間，提升設計效率\n開發 Line LIFF 會員中心前端頁面，提升會員管理體驗\n",
+            },
         ],
         resume_skills: [
             {
@@ -37,76 +38,77 @@ const Resume = () => {
                 skill_desc: [
                     {
                         skill_name: "HTML",
-                        skill_percent: 90
+                        skill_percent: 90,
                     },
                     {
                         skill_name: "CSS + tailwindCSS",
-                        skill_percent: 90
+                        skill_percent: 90,
                     },
                     {
                         skill_name: "JavaScript + NextJS",
-                        skill_percent: 85
+                        skill_percent: 85,
                     },
                     {
                         skill_name: "JavaScript + ReactJS",
-                        skill_percent: 75
+                        skill_percent: 75,
                     },
                     {
                         skill_name: "JavaScript + jQuery",
-                        skill_percent: 70
-                    }
-                ]
+                        skill_percent: 70,
+                    },
+                ],
             },
             {
                 skill_title: "後端網頁開發",
                 skill_desc: [
                     {
                         skill_name: "JavaScript + NextJS",
-                        skill_percent: 85
+                        skill_percent: 85,
                     },
                     {
                         skill_name: "php",
-                        skill_percent: 80
+                        skill_percent: 80,
                     },
                     {
                         skill_name: "MySQL + phpMyAdmin",
-                        skill_percent: 75
+                        skill_percent: 75,
                     },
                     {
                         skill_name: "NO-SQL + MongoDB",
-                        skill_percent: 75
-                    }
-                ]
+                        skill_percent: 75,
+                    },
+                ],
             },
             {
                 skill_title: "其他技能",
                 skill_desc: [
                     {
                         skill_name: "Git + GitHub",
-                        skill_percent: 80
+                        skill_percent: 80,
                     },
                     {
                         skill_name: "Linux + Ubuntu(CLI)",
-                        skill_percent: 60
-                    }
-                ]
-            }
+                        skill_percent: 60,
+                    },
+                ],
+            },
         ],
-        resume_intro: "您好，我叫 嚴太成，是一名前端專精、熟悉 Next.js (React) 的 全端網頁工程師。\n\n不以年資論能力，我更注重解決問題與打造穩健架構的能力。\我從高中開始學習網頁開發，大學期間開始接案，並協助多家企業完成專案開發：\n\n#前端網頁開發\n=＞開發 10+ 個網站前端頁面與 UI 重構，包含已商業化的 ERP 系統\n=＞專注於可維護性、共用元件開發與效能優化\n\n#後端網頁開發\n=＞獨立開發 2 家企業的完整官方網站（含後台管理系統）\n=＞為 1 家企業打造 ERP 解決方案（包含前後端架構設計）\n\n#資料庫設計\n=＞設計 5 家企業的關聯式資料庫，確保數據一致性與擴展性\n\n只要有足夠的時間與自由度，我可以完成任何網頁開發需求。\n不論是前端 UI、後端 API，還是資料庫架構，我都能獨立開發並優化整體系統。",
+        resume_intro:
+            "您好，我叫 嚴太成，是一名全端網頁工程師，主要使用 Next.js (React) 。\n\n我注重解決問題與打造穩健架構的能力。從高中開始學習網頁開發，大學期間開始接觸實務需求，並協助多家企業完成專案開發：\n\n#前端網頁開發\n=＞開發 10+ 個網站前端頁面與 UI 重構，包含已商業化的 ERP 系統\n=＞專注於可維護性、共用元件開發與效能優化\n\n#後端網頁開發\n=＞獨立開發 2 家企業的完整官方網站（含後台管理系統）\n=＞為 1 家企業打造 ERP 解決方案（包含前後端架構設計）\n\n#資料庫設計\n=＞設計 5 家企業的關聯式資料庫，確保數據一致性與擴展性\n\n只要有足夠的時間與自由度，我可以完成任何網頁開發需求。\n不論是前端 UI、後端 API，還是資料庫架構，我都能獨立開發並優化整體系統。",
         resume_freelance: [
             {
                 freelance_src: "/images/resume/chinaSteel.png",
-                freelance_alt: "China Steel Logo"
-            }
+                freelance_alt: "China Steel Logo",
+            },
         ],
         resume_certificates: [
             {
                 certificate_title: "Google Cloud 學程結業證書",
                 certificate_src: "/images/resume/google_cloud_certificate.png",
-                certificate_alt: "Google Cloud Certificate"
+                certificate_alt: "Google Cloud Certificate",
             },
-        ]
-    }
+        ],
+    };
 
     return (
         <div>
@@ -114,7 +116,7 @@ const Resume = () => {
                 bgc="/images/resume/big.jpeg"
                 bgAlt="Main resume background image."
                 name={data.resume_name}
-                btn="md" 
+                btn="md"
             />
             <Image
                 src="/images/resume/headshot.jpg"
@@ -136,9 +138,7 @@ const Resume = () => {
                             <h3 className="font-bold text-lg pt-3 text-slate-950">
                                 {edu.edu_school}
                             </h3>
-                            <h4 className="text-sm text-slate-500">
-                                {edu.edu_dept}
-                            </h4>
+                            <h4 className="text-sm text-slate-500">{edu.edu_dept}</h4>
                         </div>
                     ))}
                 </div>
@@ -195,12 +195,8 @@ const Resume = () => {
                         外語能力
                     </h2>
                     <div className="pl-5 pt-3">
-                        <h3 className="font-bold text-lg pt-3 text-slate-950">
-                            英語
-                        </h3>
-                        <h4 className="text-sm text-slate-500">
-                            TOEIC多益 795分
-                        </h4>
+                        <h3 className="font-bold text-lg pt-3 text-slate-950">英語</h3>
+                        <h4 className="text-sm text-slate-500">TOEIC多益 795分</h4>
                     </div>
                 </div>
                 <div>
@@ -251,6 +247,6 @@ const Resume = () => {
             </main>
         </div>
     );
-}
+};
 
 export default Resume;
