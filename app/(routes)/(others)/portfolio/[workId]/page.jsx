fa1cd7f132545ca6e445cb23e66415350bdb2c15
taichyy <chinzeilulu@gmail.com>
2026-01-30 16:32:17 +0800

    Fix height for missing project.

--- app/(routes)/(others)/portfolio/[workId]/page.jsx
+++ app/(routes)/(others)/portfolio/[workId]/page.jsx
@@ -13,7 +13,7 @@ const WorkIdInPortfolioPage = async ({ params }) => {
                         <Content work={work} />
                     </div>
                 ) : (
-                    <div className="text-center h-screen flex flex-col items-center justify-center">
+                    <div className="text-center flex flex-col items-center justify-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                         <h1 className="text-2xl font-bold">作品不存在</h1>
                         <p className="mt-4">您所查詢的作品不存在或已被刪除。</p>
                         <p className="mt-2">
